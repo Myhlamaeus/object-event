43b57f4f0f90a4e8a443504033fb5e8036739e07
Malte-Maurice Dreyer <dreyer.maltem+dev@gmail.com>
2015-03-08 12:38:37 +0000

    Add build scripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,9 +45,64 @@ module.exports = function (grunt) {
                 files: "<%= jshint.test.src %>",
                 tasks: ["jshint:test", "nodeunit"]
             }
+        },
+        babel: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: [{
+                    "dist/cjs.js": "<%= config.main %>.js"
+                }, {
+                    expand: true,
+                    cwd: "lib/",
+                    src: ["*.js"],
+                    dest: "dist/lib/"
+                }]
+            }
+        },
+        browserify: {
+            dist: {
+                options: {
+                    browserifyOptions: {
+                        standalone: "<%= config.global %>"
+                    }
+                },
+                files: {
+                    "dist/browser.js": "dist/cjs.js"
+                }
+            }
+        },
+        uglify: {
+            dist: {
+                options: {
+                    screwIE8: true
+                },
+                files: {
+                    "dist/<%= config.main %>.min.js": "<%= config.main %>.js"
+                }
+            },
+            distCjs: {
+                files: [{
+                    "dist/cjs.min.js": "dist/cjs.js"
+                }, {
+                    cwd: "dist/lib/",
+                    src: ["*.js"],
+                    dest: "dist/lib/",
+                    ext: ".min.js"
+                }]
+            },
+            distBrowser: {
+                files: {
+                    "dist/browser.min.js": "dist/browser.js"
+                }
+            }
         }
     });
 
-    // Default task.
     grunt.registerTask("test", ["jshint", "nodeunit"]);
+
+    grunt.task.registerTask("build:es6", ["uglify:dist"]);
+    grunt.task.registerTask("build:cjs", ["babel:dist"]);
+    grunt.task.registerTask("build:browser", ["babel:dist", "browserify:dist", "uglify:distBrowser"]);
 };
